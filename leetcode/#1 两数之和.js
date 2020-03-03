b47459002e202c1dc8a5ebb64d812c5facc065ea
Billbiubiu <1607786753@qq.com>
2020-03-03 14:53:24 +0800

    leetcode

---
+++ leetcode/#1 两数之和.js
@@ -0,0 +1,13 @@
+function toSum(nums, target) {
+  let m = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    let t = target - nums[i]
+    if (m.has(t)) {
+      return [m.get(t), i]
+    }
+    m.set(nums[i], i)
+  }
+}
+
+// 测试用例
+// console.log([2, 7, 11, 15], 9)
