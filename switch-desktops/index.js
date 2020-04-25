c992ce6d0ebcc21e29e2b90b08b3a3278cf00a61
Billbiubiu <1607786753@qq.com>
2020-04-25 20:28:35 +0800

    optimized

--- switch-desktops/index.js
+++ switch-desktops/index.js
@@ -4,23 +4,29 @@ const utils = require('./utils')
 
 const screen_height = robot.getScreenSize().height
 
+const meet_the_condition = () => {
+  let y = robot.getMousePos().y
+  if (screen_height - y <= 20 || y <= 20) {
+    return true
+  } else {
+    return false
+  }
+}
+
 ioHook.on("mousedown", event => {
   if (event.button === 4) {
-    let y = robot.getMousePos().y
-    if (screen_height - y <= 20) {
+    if (meet_the_condition()) {
       robot.keyTap('left', ['control', 'command'])
     }
   } else if (event.button === 5) {
-    let y = robot.getMousePos().y
-    if (screen_height - y <= 20) {
+    if (meet_the_condition()) {
       robot.keyTap('right', ['control', 'command'])
     }
   }
 });
-ioHook.on('mousewheel', utils.debounce(function() {
-  let y = robot.getMousePos().y
-  if (screen_height - y <= 20) {
+ioHook.on('mousewheel', utils.debounce(function () {
+  if (meet_the_condition()) {
     robot.keyTap('tab', 'alt')
   }
-}, 100))
+}, 200, true))
 ioHook.start();
